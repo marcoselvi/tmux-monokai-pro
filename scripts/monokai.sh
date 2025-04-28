35c5b31c055ca89e7e128f5749ee660d2242f852
Marco Selvi <marco.selvi@gmail.com>
2025-04-28 16:34:51 +0800

    Fix right end black border 17

--- scripts/monokai.sh
+++ scripts/monokai.sh
@@ -74,9 +74,9 @@ main()
   # Handle powerline option
   if $show_powerline; then
     left_sep="$show_left_sep"
-    left_end="  $show_left_end"
+    left_end=" $show_left_end"
     right_sep="$show_right_sep"
-    right_end="$show_right_end  "
+    right_end="$show_right_end "
   fi
 
   # start weather script in background
